41671ccbb6f4f4953408f16a5d4ff0ce671dca71
Tom Gagliano <tgaglianoll@gmail.com>
2023-06-01 13:30:32 -0400

    add ResumesShow on content file

--- src/ResumesShow.jsx
+++ src/ResumesShow.jsx
@@ -1,4 +1,4 @@
-export function Show() {
+export function ResumesShow() {
   return (
     <div>
       <div>
@@ -9,43 +9,43 @@ export function Show() {
         <p>Email: </p>
         <p>Phone Number:</p>
         <p>Short Bio:</p>
-        <p>LinkedIn URL</p>
+        <p>LinkedIn URL:</p>
         <p>Twitter Handle</p>
-        <p>Personal Blog/Website URL</p>
-        <p>Online Resume URL</p>
-        <p>Github URL</p>
-        <p>Photo</p>
+        <p>Personal Blog/Website URL:</p>
+        <p>Online Resume URL:</p>
+        <p>Github URL:</p>
+        <p>Photo:</p>
       </div>
 
       <br></br>
       <br></br>
 
       <div>
-        <h2>Experience</h2>
+        <h2>Experience:</h2>
         <p>State Date:</p>
         <p>End Date:</p>
         <p>Job Title:</p>
         <p>Company Name:</p>
-        <p>Details</p>
+        <p>Details:</p>
       </div>
 
       <br></br>
       <br></br>
 
       <div>
-        <h2>Education</h2>
+        <h2>Education:</h2>
         <p>State Date:</p>
         <p>End Date:</p>
         <p>Degree:</p>
         <p>University Name:</p>
-        <p>Details</p>
+        <p>Details:</p>
       </div>
       
       <br></br>
       <br></br>
 
       <div>
-        <h2>Skills</h2>
+        <h2>Skills:</h2>
         <p>Skill Name:</p>
       </div>
       
@@ -53,11 +53,11 @@ export function Show() {
       <br></br>
 
       <div>
-        <h2>Capstone</h2>
-        <p>Name</p>
-        <p>Description</p>
-        <p>URL</p>
-        <p>Screenshot</p>
+        <h2>Capstone:</h2>
+        <p>Name:</p>
+        <p>Description:</p>
+        <p>URL:</p>
+        <p>Screenshot:</p>
       </div>
     </div>
   );

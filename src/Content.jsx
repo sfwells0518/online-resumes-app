9b0f1e96abc0dfcf6ba01b94d733c67a423bfad6
Sam Wells <sfwells0518@gmail.com>
2023-06-01 12:31:08 -0500

    remove Routes code from Content that was not needed yet

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,21 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { ResumesIndex } from "./ResumesIndex";
-import { ResumesIndex } from "./ResumesIndex";
+
 
 export function Content() {
   return (
     <div>
       <ResumesIndex />
-      {/* <Routes>
-      <h1>Welcome to React!</h1>
-      <ResumesIndex />
-      {/* <Routes>
-        <Route path="/index" element={<Home />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/show" element={<Show />} />
-      </Routes> */}
-        <Route path="/show" element={<Show />} />
-      </Routes> */}
+
     </div>
   );
 }

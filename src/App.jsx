49fa307892e1b499238c139811f548804a58a763
Sam Wells <sfwells0518@gmail.com>
2023-06-01 11:46:03 -0500

    add header code with About and Account nav items

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { Header } from "./Header";
 import { Content } from "./Content";
-
+import { BrowserRouter } from "react-router-dom";
 import { Footer } from "./Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
 
 function App() {
   return (
     <div>
-      <Header />
-      <Content />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Content />
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }

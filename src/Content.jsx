534a525a54f9ffb0373a419c9bace0821d43f437
Alex Liew <liewtwoo42@gmail.com>
2023-06-02 13:18:37 -0400

    make logout pretty, add footer styling

--- src/Content.jsx
+++ src/Content.jsx
@@ -3,15 +3,31 @@ import { ResumesIndex } from "./ResumesIndex";
 import { ResumesShow }  from "./ResumesShow";
 import { Login } from "./Login.jsx"
 import { Signup } from "./Signup.jsx"
+import { Logout } from "./Logout.jsx"
+import axios from "axios"
+import { useState, useEffect } from "react"
 
 export function Content() {
+  const [students, setStudents] = useState([]);
+
+  const handleResumesIndex = () => {
+    console.log("handleindex photos")
+    axios.get("http://localhost:3000/students.json").then((response) => {
+      console.log(response.data)
+      setStudents(response.data)
+    })
+
+  }
+  
+  useEffect(handleResumesIndex, []);
   return (
     <div>
       <Routes>
-        <Route path="/" element={<ResumesIndex />}/>
+        <Route path="/" element={<ResumesIndex students={students}/>}/>
         <Route path="/show" element={<ResumesShow />}/>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout/>} />
       </Routes>
     </div>
   );  
